fd2ea78f3cf56ddd3df7a47256b3b22aa7b23c7a
Rudi Timmermans <rudi.timmer@posteo.be>
2026-07-04 08:34:50 +0200

    Update internal icons to a more professional design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-val appVersionName = "1.7"
+val appVersionName = "1.8"
 
 plugins {
     alias(libs.plugins.android.application)
@@ -17,7 +17,7 @@ android {
         applicationId = "com.light.lightcamera"
         minSdk = 26
         targetSdk = 37
-        versionCode = 8
+        versionCode = 9
         versionName = appVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
