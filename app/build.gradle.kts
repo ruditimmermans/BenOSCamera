ffd4e65b47f4b1b64c265d82a53a2bc968e58fc1
Rudi Timmermans <rudi.timmer@posteo.be>
2026-09-20 04:05:02 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-val appVersionName = "2.2"
+val appVersionName = "2.3"
 
 plugins {
     alias(libs.plugins.android.application)
@@ -17,7 +17,7 @@ android {
         applicationId = "com.light.lightcamera"
         minSdk = 26
         targetSdk = 37
-        versionCode = 13
+        versionCode = 14
         versionName = appVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
